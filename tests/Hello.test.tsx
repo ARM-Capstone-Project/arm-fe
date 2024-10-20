3722315cee144566410567c08dea921631b96592
goh0622 <122134041+goh0622@users.noreply.github.com>
2024-10-20 20:13:54 +0800

    Setup test environment

---
+++ tests/Hello.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+
+import Hello from '../src/Hello';
+
+describe('Hello', () => {
+   it('should render app', () => {
+      render(<Hello />);
+      expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+   });
+});
