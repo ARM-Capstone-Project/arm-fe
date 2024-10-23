417b67c1847c0b44f494c2a686678aaeaaeb9c0b
goh0622 <122134041+goh0622@users.noreply.github.com>
2024-10-23 18:26:50 +0800

    Fix error TS2345: Argument of type 'string | null'

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -39,10 +39,8 @@ export const register = (username: string, email: string, password: string) => {
 
 export const getCurrentUser = () => {
   const userStr = localStorage.getItem('user')
-  const currentUser = JSON.parse(userStr)
-
-  if (currentUser) {
-    return currentUser
+  if (userStr) {
+    return JSON.parse(userStr)
   }
   return null
 }
